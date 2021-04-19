17a0ad1d3e11cca506cd69c044bc22d7264ca73c
maanya1205 <77110811+maanya1205@users.noreply.github.com>
2021-04-19 18:52:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function getParagraph1()
+{
+    var inputs=[];
+    for (var i=1; i<=6; i++)
+    {
+        inputs.push(document.getElementById("para1_input_box_"+i).value);
+        inputs.join(".");
+    }
+    inputs.push(document.getElementById("para2_input_box_"+i).value);
+    inputs.join(".");
+}
+inputs.push(document.getElementById("para3_input_box_"+i).value);
+inputs.join(".");
+}
+inputs.push(document.getElementById("para4_input_box_"+i).value);
+inputs.join(".");
+}
+inputs.push(document.getElementById("para5_input_box_"+i).value);
+inputs.join(".");
+}
+inputs.push(document.getElementById("para6_input_box_"+i).value);
+inputs.join(".");
+}
+}
